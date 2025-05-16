02d18d2f302c66f4cbdbed7d65c7c22790294ec3
DharmAmner <areskilling1994@gmail.com>
2025-05-15 22:59:04 -0500

    Add files via upload

---
+++ DharMAIL.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo -e "\e[1;35m
+█████████████████████████████████████████████████████████████████
+█▒▒▒▒▒▒██████████▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒█████████
+█▒▒▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▄▀▄▀▒▒█▒▒▄▀▒▒█████████
+█▒▒▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▄▀▒▒█▒▒▒▒▄▀▒▒▒▒█▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒▒▒▒▒▄▀▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒███▒▒▄▀▒▒███▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▄▀▒▒███▒▒▄▀▒▒███▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒███▒▒▄▀▒▒███▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒██▒▒▒▒▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▄▀▒▒███▒▒▄▀▒▒███▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒███▒▒▄▀▒▒███▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▒▒▄▀▒▒▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█
+█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█
+█▒▒▒▒▒▒██████████▒▒▒▒▒▒█▒▒▒▒▒▒██▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+█████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████████████
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒██████████▒▒▒▒▒▒█
+█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒██▒▒▄▀▒▒█
+█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒██▒▒▄▀▒▒█
+█▒▒▄▀▒▒█████████▒▒▄▀▒▒█████████▒▒▄▀▒▒▒▒▒▒▄▀▒▒██▒▒▄▀▒▒█
+█▒▒▄▀▒▒█████████▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█
+█▒▒▄▀▒▒██▒▒▒▒▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█
+█▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█
+█▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒█████████▒▒▄▀▒▒██▒▒▄▀▒▒▒▒▒▒▄▀▒▒█
+█▒▒▄▀▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▒▒██▒▒▄▀▄▀▄▀▄▀▄▀▒▒█
+█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒██▒▒▒▒▒▒▒▒▒▒▄▀▒▒█
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒██████████▒▒▒▒▒▒█
+██████████████████████████████████████████████████████\e[0m"
+
+echo -e "\e[1;37mGENERADOR DE CORREOS DHARMA\e[0m"
+
+generate_username() {
+    prefixes=("jose" "maria" "juan" "laura" "carlos" "ana" "david" "marta")
+    suffixes=("1980" "1990" "2000" "2020" "latino" "chico" "chica" "top")
+    
+    prefix=${prefixes[$RANDOM % ${#prefixes[@]}]}
+    suffix=${suffixes[$RANDOM % ${#suffixes[@]}]}
+    
+    echo "${prefix}.${suffix}"
+}
+
+generate_emails() {
+    local domain=$1
+    local count=$2
+    
+    for ((i = 0; i < count; i++)); do
+        username=$(generate_username)
+        echo "${username}@${domain}" >> "/storage/emulated/0/emails.txt"
+    done
+}
+
+clear
+
+echo "Generador de correos electrónicos latinos al azar"
+echo "--------------------------------------------------"
+echo
+
+read -p "Ingrese la cantidad de correos a generar: " total_emails
+
+if [ $total_emails -le 0 ]; then
+    echo "La cantidad ingresada debe ser mayor que cero."
+    exit 1
+fi
+
+echo
+echo "Seleccione una opción:"
+echo "1. Generar correos con dominio Hotmail"
+echo "2. Generar correos con dominio Gmail"
+echo "3. Generar correos con dominio Outlook"
+echo "4. Generar correos con diferentes dominios"
+echo
+read -p "Opción: " option
+
+case $option in
+    1) generate_emails "hotmail.com" $total_emails ;;
+    2) generate_emails "gmail.com" $total_emails ;;
+    3) generate_emails "outlook.com" $total_emails ;;
+    4) generate_emails "hotmail.com" $((total_emails / 3))
+       generate_emails "gmail.com" $((total_emails / 3))
+       generate_emails "outlook.com" $((total_emails - 2 * (total_emails / 3))) ;;
+    *) echo "Opción inválida."; exit 1 ;;
+esac
+
+echo "Correos generados exitosamente. Se han guardado en /storage/emulated/0/emails.txt"
